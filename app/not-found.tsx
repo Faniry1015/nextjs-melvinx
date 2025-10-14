f06f43721e37e0428ac61c0d4e889986e9b8b619
Faniry1015 <frandriaharimino@yahoo.com>
2025-10-14 14:58:52 +0300

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function notfound() {
   return (
     <PagesLayout title="Page non trouvée">
       <Card className="w-full flex flex-col items-center justify-center p-10">
-        <p className="mb-6">Désolé, la page que vous recherchez n’;existe pas.</p>
+        <p className="mb-6">Désolé, la page que vous recherchez n’existe pas.</p>
         <Link href="/">
           <Button>Retour à l’accueil</Button>
         </Link>
