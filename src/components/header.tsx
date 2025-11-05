ae9ba8ad054022f2b182f2e06adbf657af531180
Faniry1015 <frandriaharimino@yahoo.com>
2025-11-05 10:17:21 +0300

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import { NavMenu } from './NavigationMenu'
-import { ModeToggle } from './mode-toggle'
 
 export const Header = () => {
     return (
         <div>
-            <NavMenu />
+            <NavMenu /> 
         </div>
     )
 }
